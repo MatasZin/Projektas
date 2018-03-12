b96655eae5141cf2fc457887c50eb07803c604c2
MatasZin <matas.zinka@ktu.lt>
2018-03-12 23:54:16 +0200

    auth

---
+++ Models/Authentication.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matas
+ * Date: 3/12/18
+ * Time: 10:58 PM
+ */
+
+namespace Models;
+use Models\User;
+use Models\UsersContainer;
+
+class Authentication
+{
+    private $users;
+    public function __construct(UsersContainer $usersContainer)
+    {
+        $this->users = $usersContainer;
+    }
+
+    public function check($email, $password)
+    {
+        if (!empty($_SESSION["email"])) {
+            return true;
+        }
+        $user = $this->users->getUser($email);
+        if ($user != null) {
+            $this->login($user);
+            return $user->getPassword() === (string)$password;
+        }
+        return false;
+    }
+    public function logout()
+    {
+        $_SESSION["email"] = null;
+    }
+    private function login(User $member)
+    {
+        $_SESSION["email"] = $member->getEmail();
+    }
+}
