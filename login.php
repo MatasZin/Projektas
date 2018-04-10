72ad28a2ae967d62495ac88f5235bbd387b322bb
Paulius Adamonis <paulius.adamonis@ktu.edu>
2018-04-10 21:30:12 +0300

    Installed Symfony framework for our project

--- login.php
+++
@@ -1,8 +0,0 @@
-<form action="index.php" method="post">
-    <p> <label for="email">E-mail:</label>
-        <input id="email" class="simple-input" type="text" name="email" />
-        <label for="pass">Password:</label>
-        <input id="pass" class="simple-input" type="password" name="pass" /></p>
-    <p><input class="modern" type="submit" value="Log in" name="login" />
-        <input type="button" class="modern" value="Sign up" onclick="location.href='register.php';" /></p>
-</form>
